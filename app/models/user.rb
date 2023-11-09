ea68dae35df6c14ed0d9a913d991888edb180333
lada <lavacashy.sinkevich@gmail.com>
2023-11-09 15:53:49 +0100

    Initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,35 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+         followability
+         has_many :posts
+         has_many :likes
+         has_many :comments
+         has_one_attached :avatar
+         before_create :randomize_id
+
+    def unfollow(user)
+      followerable_relationships.where(followable_id: user.id).destroy_all
+    end
+
+    def self.ransackable_attributes(auth_object = nil)
+      %w[username email]
+    end
+
+    private
+
+    def randomize_id
+      begin
+        self.id = SecureRandom.random_number(1_000_000_000)
+      end while User.where(id: self.id).exists?
+    end
+
+         enum role: [:user, :admin]
+         after_initialize :set_default_role, :if => :new_record?
+         def set_default_role
+         self.role ||= :admin
+         end
+end
